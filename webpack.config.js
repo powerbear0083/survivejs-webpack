91ba7ff014592310c9f9e9c2ee756c85396861f1
yinhsun <powerbear0083@gmail.com>
2020-11-08 17:43:33 +0800

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
+const path = require('path');
 const { mode } = require("webpack-nano/argv");
 const {
     MiniHtmlWebpackPlugin, // dist 底下沒有 index.html，可以透過此 plugin 產生 html
 } = require("mini-html-webpack-plugin");
 
 module.exports = {
+    watch: mode === "development",
+    entry: {
+        main: './src/index.js'
+    },
     mode,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: true,
+        port: 9000
+    },
     plugins: [
         new MiniHtmlWebpackPlugin({
             context: {
